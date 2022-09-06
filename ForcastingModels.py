1957ce610a5b853ceb6441fca1a38008fcfe88b0
Joshua Tampoe-holmes <80101612+JoshT-H@users.noreply.github.com>
2022-09-06 08:02:55 -0700

    Update ForcastingModels.py

--- ForcastingModels.py
+++ ForcastingModels.py
@@ -8,28 +8,32 @@ from statsmodels.tsa.statespace.sarimax import SARIMAX
 warnings.filterwarnings('ignore')
 
 
-
-def met_data_extractor(df):
+def met_data_extractor(df, split):
     """
-    DataExtractor for Met Office Dataset
+    DataExtractor for Met Office Dataset: Monthly vs. Quarterly vs. Annual
     """
-    dfMod = df.drop(['year','win', 'spr', 'sum', 'aut', 'ann'], axis = 1)
-    x = 0
+    if  split == "monthly":
+        dfMod = df.drop(['year','win', 'spr', 'sum', 'aut', 'ann'], axis = 1)
+        x = 0
+        year = df.iloc[:,0]
+        # Create a Datatime Range for Index
+        index = pd.date_range(start = str(year.min()), end = str(year.max()), freq = 'MS')
+        dataFrame = pd.DataFrame({'date': index})
+        # Create a Series to Iterate through Data
+        col = pd.Series()
 
-    # Create a Datatime Range for Index
-    year = df.iloc[:,0]
-    index = pd.date_range(start = str(year.min()), end = str(year.max()), freq = 'MS')
-    dataFrame = pd.DataFrame({'date': index})
+        while x < len(df):
+            monthlyData = pd.Series([ _ for _ in dfMod.iloc[x]])
+            col = pd.concat([col, monthlyData], ignore_index = True)
+            x += 1
+        dataFrame['rainfall']  = col
+        dataFrame['date'] = pd.to_datetime(dataFrame['date'])
+        dataFrame = dataFrame.set_index('date')
+    elif split == "quarterly":
+        pass
+    elif split == "Annual":
+        pass
 
-    # Create a Series to Iterate through Data
-    col = pd.Series()
-    while x < len(df):
-        monthlyData = pd.Series([ _ for _ in dfMod.iloc[x]])
-        col = pd.concat([col, monthlyData], ignore_index = True)
-        x += 1
-    dataFrame['rainfall']  = col
-    dataFrame['date'] = pd.to_datetime(dataFrame['date'])
-    dataFrame = dataFrame.set_index('date')
     return dataFrame.dropna()
 
 def AutoAIMAX(df, seasonal, m):
@@ -46,7 +50,7 @@ def traintest_split(df, months):
 if __name__ == '__main__':
 
     df  = pd.read_csv("Dataset.csv")
-    df  = met_data_extractor(df)
+    df  = met_data_extractor(df, split = "monthly")
 
     # Current forcasting has been set to a year
     train, test = traintest_split(df, months = 12);
@@ -56,7 +60,9 @@ if __name__ == '__main__':
     # seasonal.plot()
 
     # Obtaining Optimal Model Param.
-    AutoAIMAX(df, seasonal = True, m = 12)
+    AutoAIMAX(df, seasonal = True, m = 15)
+
+    #df.describe()
 
     #model = SARIMAX(train['rainfall'], order = (2,1,0), seasonal_order = (2,0,1,4), enfore_invertibility = True)
     # result.fit()
