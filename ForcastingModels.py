edef7a4db1ae859f60d1d7311945e65ac414c909
Joshua Tampoe-holmes <80101612+JoshT-H@users.noreply.github.com>
2022-09-07 07:35:49 -0700

    Update ForcastingModels.py

--- ForcastingModels.py
+++ ForcastingModels.py
@@ -4,74 +4,90 @@ import warnings
 from statsmodels.tsa.seasonal import seasonal_decompose
 from pmdarima import auto_arima
 from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tools.eval_measures import mse, rmse, meanabs
+from AIMAX import ARIMAX
 
 warnings.filterwarnings('ignore')
 
-
 def met_data_extractor(df, split):
     """
-    DataExtractor for Met Office Dataset: Monthly vs. Quarterly vs. Annual
+    Reformates Dataframe
     """
+    # Creating DataFrame based on number of Years
+    year = df.iloc[:,0]
+
     if  split == "monthly":
         dfMod = df.drop(['year','win', 'spr', 'sum', 'aut', 'ann'], axis = 1)
-        x = 0
-        year = df.iloc[:,0]
-        # Create a Datatime Range for Index
         index = pd.date_range(start = str(year.min()), end = str(year.max()), freq = 'MS')
-        dataFrame = pd.DataFrame({'date': index})
-        # Create a Series to Iterate through Data
-        col = pd.Series()
-
-        while x < len(df):
-            monthlyData = pd.Series([ _ for _ in dfMod.iloc[x]])
-            col = pd.concat([col, monthlyData], ignore_index = True)
-            x += 1
-        dataFrame['rainfall']  = col
-        dataFrame['date'] = pd.to_datetime(dataFrame['date'])
-        dataFrame = dataFrame.set_index('date')
     elif split == "quarterly":
-        pass
-    elif split == "Annual":
-        pass
+        dfMod = df.iloc[:,13:17]
+    elif split == "annual":
+        index = pd.date_range(start = str(year.min()), end = str(year.max()), freq = 'Y')
+        dfMod = df.iloc[:,17]
+
+    dataFrame = pd.DataFrame({'date': index})
+
+    # Create a Series to store data from orginal dataframe
+    if split != "annual":
+        x = 0
+        col = pd.Series()
+        while x < len(df):
+            rainfallData = pd.Series([ _ for _ in dfMod.iloc[x]])
+            col = pd.concat([col, rainfallData], ignore_index = True)
+            x += 1
+        # Inserting original Data into Create Frame
+        dataFrame['rainfall']  = col
+    else:
+        dataFrame['rainfall'] = dfMod
+
+    dataFrame['date'] = pd.to_datetime(dataFrame['date'])
+    dataFrame = dataFrame.set_index('date')
 
     return dataFrame.dropna()
 
-def AutoAIMAX(df, seasonal, m):
-    return print(auto_arima(df, seasonal = seasonal, m = m).summary())
 
-
-
-def traintest_split(df, months):
-
-    train = df.iloc[:len(df)-months]
-    test  = df.iloc[len(df)-months:]
+def traintest_split(df, duration):
+    train = df.iloc[:len(df)-duration]
+    test  = df.iloc[len(df)-duration:]
     return train, test
+
+
 
 if __name__ == '__main__':
 
     df  = pd.read_csv("Dataset.csv")
-    df  = met_data_extractor(df, split = "monthly")
+    df  = met_data_extractor(df, split = "annual")
 
     # Current forcasting has been set to a year
-    train, test = traintest_split(df, months = 12);
+    train, test = traintest_split(df, duration = 10);
 
     # Check if Seasonality Component Exisits
     seasonal = seasonal_decompose(df['rainfall'])
     # seasonal.plot()
 
     # Obtaining Optimal Model Param.
-    AutoAIMAX(df, seasonal = True, m = 15)
+    seasonal = True
+    seasonalPeriod = 12
+    arimaxObject = ARIMAX(df, seasonal, seasonalPeriod, train['rainfall'], test)
 
-    #df.describe()
+    # Optimal Model for Annual rainfall
+    # order = (0, 1, 1)
+    # seasonal order = (0, 0, 1, 12)
 
-    #model = SARIMAX(train['rainfall'], order = (2,1,0), seasonal_order = (2,0,1,4), enfore_invertibility = True)
-    # result.fit()
-    # start = len(train)
-    #end   =  len(train) + len(test) -1
-    #prediction = results.predict(start, end).rename('SARIMA Model')
+    # Obtain prediction AIMAX Model
+    #predictions = arimaxObject.seasonalAIMAX((0, 1, 1), (0, 0, 1, 12))
+
+    # Evaluationg Predcitions
 
 
+    # Reccurent Neural Network
+    ninput = 2 # Lenght of Training Object i.e., first two points # should be Around 12
+    nfeatures = 1 # Predict the Third
 
+    
 
-    #df.plot()
-    #plt.pyplot.show()
+    
+
+    # df.describe()
+    # df.plot()
+    plt.pyplot.show()
