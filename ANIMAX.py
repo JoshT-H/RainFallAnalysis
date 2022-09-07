6ef6601e0701e3e2aedabc3e5787cec470271238
Joshua Tampoe-holmes <80101612+JoshT-H@users.noreply.github.com>
2022-09-07 07:36:58 -0700

    Create ANIMAX.py

---
+++ ANIMAX.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import matplotlib as plt
+import warnings
+from statsmodels.tsa.seasonal import seasonal_decompose
+from pmdarima import auto_arima
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tools.eval_measures import mse, rmse, meanabs
+
+
+class ARIMAX:
+    def __init__(self, df, seasonal, period, train, test):
+        self.seasonal = seasonal
+        self.period  = period
+        self.df = df
+        self.train = train
+        self.test  = test
+
+    def autoAIMAX(self):
+        print(auto_arima(self.df, seasonal = self.seasonal, m = self.period).summary())
+
+    def seasonalAIMAX(self, order, seasonal_order):
+        model = SARIMAX(self.train, order = order, seasonal_order =seasonal_order, enfore_invertibility = True)
+        results = model.fit() ##############################################
+        start = len(self.train)
+        end   =  len(self.train) + len(self.test) - 1
+        predicition = results.predict(start, end).rename('SARIMA Model')
+        return predictions
+
+    def mse_aima(self):
+        print(f'The ARIMAX MSE is {meanabs(predictions, test)}')
+
+    def aima_plots(self, predictions):
+        self.train.plot()
+        predictions.plot(legend = True)
+        self.test.plot(legend = True)
+
+
+if __name__ =='__main__':
+    pass
